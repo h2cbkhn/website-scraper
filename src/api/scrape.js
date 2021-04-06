7fd1a7d24870cf69d10eabc0b0a08a3250f02a74
hhcbkhn <minhpv.bkhn@gmail.com>
2021-04-06 14:09:24 +0700

    no message

--- src/api/scrape.js
+++ src/api/scrape.js
@@ -7,35 +7,12 @@ export default {
     const url = req.query.url || 'themes'
     const options = {
       urls: [
-        'http://www.ansonika.com/vanno/',
-        // 'http://www.ansonika.com/vanno/index-2.html',
-        // 'http://www.ansonika.com/vanno/grid-listings-filterstop.html',
-        // 'http://www.ansonika.com/vanno/grid-listings-filterscol.html',
-        // 'http://www.ansonika.com/vanno/row-listings-filterscol.html',
-        // 'http://www.ansonika.com/vanno/reviews-page.html',
-        // 'http://www.ansonika.com/vanno/write-review.html',
-        // 'http://www.ansonika.com/vanno/confirm.html',
-        // 'http://www.ansonika.com/vanno/user-dashboard.html',
-        // 'http://www.ansonika.com/vanno/user-settings.html',
-        // 'http://www.ansonika.com/vanno/pricing.html',
-        // 'http://www.ansonika.com/vanno/companies-landing.html',
-        // 'http://www.ansonika.com/vanno/all-categories.html',
-        // 'http://www.ansonika.com/vanno/category-companies-listings-filterstop.html',
-        // 'http://www.ansonika.com/vanno/blog.html',
-        // 'http://www.ansonika.com/vanno/login.html',
-        // 'http://www.ansonika.com/vanno/register.html',
-        // 'http://www.ansonika.com/vanno/about.html',
-        // 'http://www.ansonika.com/vanno/help.html',
-        // 'http://www.ansonika.com/vanno/faq.html',
-        // 'http://www.ansonika.com/vanno/contacts.html',
-        // 'http://www.ansonika.com/vanno/icon-pack-1.html',
-        // 'http://www.ansonika.com/vanno/icon-pack-2.html',
-        // 'http://www.ansonika.com/vanno/icon-pack-3.html',
-        // 'http://www.ansonika.com/vanno/icon-pack-4.html',
-        // 'http://www.ansonika.com/vanno/404.html',
-
+        // 'http://www.ansonika.com/vanno/',
+        {url: 'http://www.ansonika.com/vanno/confirm.html', filename: 'confirm.html'},
+        {url: 'http://www.ansonika.com/vanno/category-companies-listings-filterstop.html', filename: 'category-companies-listings-filterstop.html'},
+        {url: 'http://www.ansonika.com/vanno/help.html', filename: 'help.html'},
       ],
-      directory: '/minhpv/website-scraper/themes/vanno/index',
+      directory: '/minhpv/website-scraper/themes/tmp',
       plugins: [ 
         new PuppeteerPlugin({
           launchOptions: { headless: false }, /* optional */
